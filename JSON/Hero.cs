1921d2b23f6fdd6815c8d6f4b684f5526feb0744
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-10-19 13:08:21 +0200

    first commit

---
+++ JSON/Hero.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Hero
+{
+    public string name {get; set;}
+    public string description {get; set;}
+    public string ability {get; set;}
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+string fileName = "Character.json";
+Hero hero = JsonSerializer.Deserialize<Hero>(File.ReadAllText(fileName));
+
+Hero Hero = new Hero();
+
+Game();
+void Game() {
+    Console.Clear();
+    Console.WriteLine("Hello Player!\nWould you like to create a new character?\nType '1' to create a character\nType '2' to load an old character");
+    string input = Console.ReadLine();
+    Console.Clear();
+    if (input == "1") {
+        Console.WriteLine("Are you sure?\nThis action will overwrite the old character's info\nType 'Y' To continue");
+        input = Console.ReadLine().ToLower();
+        if (input == "y") {
+            Console.WriteLine("Character Name:");
+            Hero.name = Console.ReadLine();
+            Console.WriteLine("Character Description:");
+            Hero.description = Console.ReadLine();
+            Console.WriteLine("Character Ability:");
+            Hero.ability = Console.ReadLine();
+            string jsonString = JsonSerializer.Serialize<Hero>(Hero);
+            File.WriteAllText(fileName, jsonString);
+        }
+        else {
+            Game();
+        }
+    }
+    else if (input == "2") {
+        if (hero.name == null) {
+            Game();
+        }
+        else {
+            Console.WriteLine(hero.description);
+            Console.ReadLine();
+        }
+    }
+
+    else {
+        Game();
+    } 
+}
